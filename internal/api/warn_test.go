56ce81b566ff309b0b145fd0b118639fc5cb6551
Cian Johnston <cian@coder.com>
2021-08-30 10:35:31 +0000

    add api.WarnResult

---
+++ internal/api/warn_test.go
@@ -0,0 +1,17 @@
+package api_test
+
+import (
+	"testing"
+
+	"cdr.dev/slog/sloggers/slogtest/assert"
+	"github.com/cdr/coder-doctor/internal/api"
+)
+
+func TestWarnResult(t *testing.T) {
+	t.Parallel()
+	res := api.WarnResult("check-name", "something bad happened")
+
+	assert.Equal(t, "name matches", "check-name", res.Name)
+	assert.Equal(t, "state matches", api.StateWarning, res.State)
+	assert.Equal(t, "summary matches", "something bad happened", res.Summary)
+}
